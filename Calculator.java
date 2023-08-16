42ae81c40503158074950c6cfd48bd9b10baa455
Wer der Sonne entgegengeht, hat den Schatten hinter sich <henrik.seiffert@gmail.com>
2023-08-16 16:43:37 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
         double num1 = scanner.nextDouble();
 
         System.out.print("Geben Sie die zweite Zahl ein: ");
-        double num2 = scanner.nextDoublebb();
+        double num2 = scanner.nextDouble();
 
         System.out.print("Geben Sie die Operation ein (+, -, *, /): ");
         char operation = scanner.next().charAt(0);
