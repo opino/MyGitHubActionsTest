da8909b5d6215eb6b9f10859dd4f2fdf422122f5
Wer der Sonne entgegengeht, hat den Schatten hinter sich <henrik.seiffert@gmail.com>
2023-08-16 15:11:05 +0200

    Update Calculator.java
    calculate funktion für JUnit tests

--- Calculator.java
+++ Calculator.java
@@ -3,16 +3,28 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //creating a error nextDouble() -> 11
+
         System.out.print("Geben Sie die erste Zahl ein: ");
         double num1 = scanner.nextDouble();
 
         System.out.print("Geben Sie die zweite Zahl ein: ");
-        double num2 = scanner.nextDouble11;
+        double num2 = scanner.nextDouble();
 
         System.out.print("Geben Sie die Operation ein (+, -, *, /): ");
         char operation = scanner.next().charAt(0);
 
+        double result = calculate(num1, num2, operation);
+
+        if (Double.isNaN(result)) {
+            System.out.println("Ungültige Operation oder Division durch Null.");
+        } else {
+            System.out.println("Ergebnis: " + result);
+        }
+
+        scanner.close();
+    }
+
+    public static double calculate(double num1, double num2, char operation) {
         double result;
 
         switch (operation) {
@@ -29,17 +41,13 @@ public class Calculator {
                 if (num2 != 0) {
                     result = num1 / num2;
                 } else {
-                    System.out.println("Division durch Null ist nicht erlaubt.");
-                    return;
+                    result = Double.NaN; // Spezieller Wert für ungültige Berechnung
                 }
                 break;
             default:
-                System.out.println("Ungültige Operation.");
-                return;
+                result = Double.NaN;
         }
 
-        System.out.println("Ergebnis: " + result);
-
-        scanner.close();
+        return result;
     }
 }
