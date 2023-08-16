9bc2bb7dc2691136864000f9ff8086b05c118629
Wer der Sonne entgegengeht, hat den Schatten hinter sich <henrik.seiffert@gmail.com>
2023-08-16 12:10:50 +0200

    Create Calculator.java
    Class except 2 numbers and a math operation, return the result

---
+++ Calculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Geben Sie die erste Zahl ein: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Geben Sie die zweite Zahl ein: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Geben Sie die Operation ein (+, -, *, /): ");
+        char operation = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Division durch Null ist nicht erlaubt.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Ungültige Operation.");
+                return;
+        }
+
+        System.out.println("Ergebnis: " + result);
+
+        scanner.close();
+    }
+}
