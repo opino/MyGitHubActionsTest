ac3860964b4b6090162071f16ac6333db6577537
Wer der Sonne entgegengeht, hat den Schatten hinter sich <henrik.seiffert@gmail.com>
2023-08-16 13:19:54 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,7 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
         //creating a error nextDouble() -> 11
         System.out.print("Geben Sie die erste Zahl ein: ");
-        double num1 = scanner.nextDouble33;
+        double num1 = scanner.nextDouble();
 
         System.out.print("Geben Sie die zweite Zahl ein: ");
         double num2 = scanner.nextDouble();
